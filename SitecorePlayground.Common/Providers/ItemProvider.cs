e7e1e560153172038fe3d303e4cf890a5f225ca8
Marc Duiker <marcduiker@gmail.com>
2014-11-12 00:10:43 +0100

    Implemented ItemProvider

--- SitecorePlayground.Common/Providers/ItemProvider.cs
+++ SitecorePlayground.Common/Providers/ItemProvider.cs
@@ -1,7 +1,22 @@
-﻿namespace SitecorePlayground.Common.Providers
-{
-    public class ItemProvider
-    {
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using SitecorePlayground.Common.Adapters;
+using SitecorePlayground.Common.Interfaces.Adapters;
+using SitecorePlayground.Common.Interfaces.Providers;
 
+namespace SitecorePlayground.Common.Providers
+{
+    public class ItemProvider : IItemProvider
+    {
+        public Item GetItem(ID itemId)
+        {
+            return Sitecore.Context.Database.GetItem(itemId);
+        }
+
+        public IItem GetItemAdapter(ID itemId)
+        {
+            var item = Sitecore.Context.Database.GetItem(itemId);
+            return item != null ? new ItemAdapter(item) : null;
+        }
     }
 }
